b5f11eb050989f55a1ae223ec77790a520a13cc3
Legolas1148 <niteeshrawal1@gmail.com>
2024-08-01 18:55:26 -0400

    Added Database Context configuration settings for connecting database

---
+++ Final_Project_Group_3/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project_Group_3.Data
+{
+    public class TeamProjectContext : DbContext
+    {
+        public TeamProjectContext(DbContextOptions<TeamProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers{ get; set; }
+        public DbSet<Hobby> Hobbies{ get; set; }
+        public DbSet<MGenre> MusicGenre{ get; set; }
+        public DbSet<Foods> FavoriteFoods{ get; set; }
+    } 
+}
