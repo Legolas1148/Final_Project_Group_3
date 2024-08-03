03f2c76ad2c9e1e48b442d7e508c5c6347dc5374
Legolas1148 <niteeshrawal1@gmail.com>
2024-08-03 10:55:31 -0400

    Added initial dataset for the database

--- Final_Project_Group_3/Data/SeedData.cs
+++ Final_Project_Group_3/Data/SeedData.cs
@@ -25,6 +25,27 @@ namespace Final_Project_Group_3.Data
                         CollegeProgram = "Information Technology",
                         YearInProgram = "Sophomore"
                     }
+                    ,new TeamMember
+                    {
+                        FullName = "Grant Perry",
+                        Birthdate = new DateTime(2003, 5, 8),
+                        CollegeProgram = "Information Technology",
+                        YearInProgram = "Sophomore"
+                    },
+                    new TeamMember
+                    {
+                        FullName = "Taka Kiuchi",
+                        Birthdate = new DateTime(1999, 1, 1),
+                        CollegeProgram = "Information Technology",
+                        YearInProgram = "Sophomore"
+                    },
+                    new TeamMember
+                    {
+                        FullName = "Kshitij Maurya",
+                        Birthdate = new DateTime(1999, 1, 1),
+                        CollegeProgram = "Information Technology",
+                        YearInProgram = "Sophomore"
+                    }
                 );
 
                 context.Hobbies.AddRange(
@@ -36,6 +57,33 @@ namespace Final_Project_Group_3.Data
                         YearsPracticed = 15,
                         Category = "Indoor",
                         TeamMemberId = 1
+                    },
+                    new Hobby
+                    {
+                        FullName = "Grant Perry",
+                        HobbyName = "Watching Movies",
+                        Description = "I like to watch movies in my free time.",
+                        YearsPracticed = 10,
+                        Category = "Indoor",
+                        TeamMemberId = 2
+                    },
+                    new Hobby
+                    {
+                        FullName = "Taka Kiuchi",
+                        HobbyName = "Playing Soccer",
+                        Description = "I like to play soccer in my free time.",
+                        YearsPracticed = 10,
+                        Category = "Outdoor",
+                        TeamMemberId = 3
+                    },
+                    new Hobby
+                    {
+                        FullName = "Kshitij Maurya",
+                        HobbyName = "Playing Cricket",
+                        Description = "I like to play cricket in my free time.",
+                        YearsPracticed = 10,
+                        Category = "Outdoor",
+                        TeamMemberId = 4
                     }
                 );
 
@@ -47,6 +95,30 @@ namespace Final_Project_Group_3.Data
                         GenreDescription = "Classical music is a genre of music that is typically composed in a more traditional style.",
                         Popularity = 5,
                         TeamMemberId = 1
+                    },
+                    new MGenre
+                    {
+                        FullName = "Grant Perry",
+                        MusicGenre = "Rock",
+                        GenreDescription = "Rock music is a genre of music that is typically composed with electric guitars and drums.",
+                        Popularity = 5,
+                        TeamMemberId = 2
+                    },
+                    new MGenre
+                    {
+                        FullName = "Taka Kiuchi",
+                        MusicGenre = "Jazz",
+                        GenreDescription = "Jazz music is a genre of music that is typically composed with saxophones and trumpets.",
+                        Popularity = 5,
+                        TeamMemberId = 3
+                    },
+                    new MGenre
+                    {
+                        FullName = "Kshitij Maurya",
+                        MusicGenre = "Pop",
+                        GenreDescription = "Pop music is a genre of music that is typically composed with catchy tunes and lyrics.",
+                        Popularity = 5,
+                        TeamMemberId = 4
                     }
                 );
 
@@ -58,7 +130,31 @@ namespace Final_Project_Group_3.Data
                         FavoriteFoodName = "Mac and Cheese",
                         Type = "Fast Food",
                         Calories = 300
-                    }
+                    },
+                    new Foods
+                    {
+                        TeamMemberId = 2,
+                        FullName = "Grant Perry",
+                        FavoriteFoodName = "Pancakes",
+                        Type = "Fast Food",
+                        Calories = 300
+                    },
+                    new Foods
+                    {
+                        TeamMemberId = 3,
+                        FullName = "Taka Kiuchi",
+                        FavoriteFoodName = "Burrito",
+                        Type = "Fast Food",
+                        Calories = 300
+                    },
+                    new Foods
+                    {
+                        TeamMemberId = 4,
+                        FullName = "Kshitij Maurya",
+                        FavoriteFoodName = "Taco",
+                        Type = "Fast Food",
+                        Calories = 300
+                    }   
                 );
 
                 context.SaveChanges();
