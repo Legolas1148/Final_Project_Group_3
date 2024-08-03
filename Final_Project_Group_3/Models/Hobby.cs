6c6bd834e3b4e7bfd75dcfc8d51219c61804c9e9
Taka <tk512003@gmail.com>
2024-08-03 02:06:52 -0400

    Update hobby table

--- Final_Project_Group_3/Models/Hobby.cs
+++ Final_Project_Group_3/Models/Hobby.cs
@@ -4,17 +4,15 @@ namespace Final_Project_Group_3.Models
 {
     public class Hobby
     {
-        [Key]
-        public int Id { get; set; }
-
-        public string FullName { get; set; }
-
-        public string HobbyName { get; set; }
-
+        [Key] 
+        public int TeamMemberId { get; set; }
+        
+        public string? FullName { get; set; }
+        
+        public string? HobbyName { get; set; }
+        
         public string? Description { get; set; }
         
-        public string SkillLevel { get; set; }
-       
-        public string Category { get; set; }
+        public int YearsPracticed { get; set; }
     }
 }
